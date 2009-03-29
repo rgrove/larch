f16a12e9abc912faf210445eb36408118bc11c28
Ryan Grove <ryan@wonko.com>
2009-03-29 15:30:28 -0700

    Fuck you, threads. You're not worth the trouble. And fuck you too, Net::IMAP.

--- lib/larch.rb
+++ lib/larch.rb
@@ -11,6 +11,7 @@ require 'uri'
 
 require 'larch/errors'
 require 'larch/imap'
+require 'larch/imap/mailbox'
 require 'larch/logger'
 require 'larch/version'
 
@@ -29,123 +30,52 @@ module Larch
 
     # Copies messages from _source_ to _dest_ if they don't already exist in
     # _dest_. Both _source_ and _dest_ must be instances of Larch::IMAP.
-    def copy(source, dest)
+    def copy_folder(source, dest)
       raise ArgumentError, "source must be a Larch::IMAP instance" unless source.is_a?(IMAP)
       raise ArgumentError, "dest must be a Larch::IMAP instance" unless dest.is_a?(IMAP)
-
-      msgq  = SizedQueue.new(8)
-      mutex = Mutex.new
 
       @copied = 0
       @failed = 0
       @total  = 0
 
-      @log.info "copying messages from #{source.uri} to #{dest.uri}"
+      source_mb_name = source.uri_mailbox || 'INBOX'
+      dest_mb_name   = dest.uri_mailbox || 'INBOX'
+
+      @log.info "copying messages from #{source.host}/#{source_mb_name} to #{dest.host}/#{dest_mb_name}"
 
       source.connect
       dest.connect
 
-      source_thread = Thread.new do
+      source_mb = source.mailbox(source_mb_name)
+      dest_mb   = dest.mailbox(dest_mb_name)
+
+      @total = source_mb.length
+
+      source_mb.each do |id|
+        next if dest_mb.has_message?(id)
+
         begin
-          source.scan_mailbox
-          mutex.synchronize { @total = source.length }
+          msg = source_mb.peek(id)
 
-          source.each do |id|
-            next if dest.has_message?(id)
-
-            begin
-              Thread.current[:fetching] = true
-              msgq << source.peek(id)
-              Thread.current[:fetching] = false
-
-            rescue Larch::IMAP::Error => e
-              # TODO: Keep failed message envelopes in a buffer for later output?
-              mutex.synchronize { @failed += 1 }
-              @log.error e.message
-              next
-            end
+          if msg.envelope.from
+            env_from = msg.envelope.from.first
+            from = "#{env_from.mailbox}@#{env_from.host}"
+          else
+            from = '?'
           end
 
-        rescue Larch::WatchdogException => e
-          Thread.current[:fetching] = false
-          @log.debug "#{source.username}@#{source.host}: watchdog exception"
-          source.noop
-          retry
+          @log.info "copying message: #{from} - #{msg.envelope.subject}"
 
-        rescue => e
-          @log.fatal "#{source.username}@#{source.host}: #{e.class.name}: #{e.message}"
-          Kernel.abort
-
-        ensure
-          msgq << :finished
-        end
-      end
-
-      dest_thread = Thread.new do
-        begin
-          dest.scan_mailbox
-
-          while msg = msgq.pop do
-            break if msg == :finished
-
-            if msg.envelope.from
-              env_from = msg.envelope.from.first
-              from = "#{env_from.mailbox}@#{env_from.host}"
-            else
-              from = '?'
-            end
-
-            @log.info "copying message: #{from} - #{msg.envelope.subject}"
-
-            Thread.current[:last_id] = msg.id
-            dest << msg
-
-            mutex.synchronize { @copied += 1 }
-          end
+          dest_mb << msg
+          @copied += 1
 
         rescue Larch::IMAP::Error => e
-          mutex.synchronize { @failed += 1 }
+          # TODO: Keep failed message envelopes in a buffer for later output?
+          @failed += 1
           @log.error e.message
-          retry
-
-        rescue Larch::WatchdogException => e
-          Thread.current[:last_id] = nil
-          @log.debug "#{dest.username}@#{dest.host}: watchdog exception"
-          dest.noop
-          retry
-
-        rescue => e
-          @log.fatal "#{dest.username}@#{dest.host}: #{e.class.name}: #{e.message}"
-          Kernel.abort
+          next
         end
       end
-
-      watchdog_thread = Thread.new do
-        source_flags = 0
-        dest_flags   = 0
-        dest_lastid  = nil
-
-        loop do
-          sleep 10
-
-          if msgq.length == 0 && source_thread[:fetching] && (source_flags += 1) > 1
-              source_flags = 0
-              source_thread.raise(WatchdogException)
-          end
-
-          if dest_thread[:last_id]
-            if dest_lastid == dest_thread[:last_id] && (dest_flags += 1) > 2
-              dest_flags  = 0
-              dest_lastid = nil
-              dest_thread.raise(WatchdogException)
-            else
-              dest_lastid = dest_thread[:last_id]
-            end
-          end
-        end
-      end
-
-      dest_thread.join
 
       source.disconnect
       dest.disconnect
