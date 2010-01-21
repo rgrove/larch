12c6e71f2a204cb2e2a0e185628026971bb78c64
Ryan Grove <ryan@wonko.com>
2010-01-20 21:37:47 -0800

    Bump version to 1.1.0.dev.20100120.

--- lib/larch/version.rb
+++ lib/larch/version.rb
@@ -1,6 +1,6 @@
 module Larch
   APP_NAME      = 'Larch'
-  APP_VERSION   = '1.1.0.dev.20091206'
+  APP_VERSION   = '1.1.0.dev.20100120'
   APP_AUTHOR    = 'Ryan Grove'
   APP_EMAIL     = 'ryan@wonko.com'
   APP_URL       = 'http://github.com/rgrove/larch/'
