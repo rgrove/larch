9dee1f5d345c7d8361739cba06e70c49c02b0642
Ryan Grove <ryan@wonko.com>
2011-06-26 21:21:15 -0700

    Release 1.1.1.

--- lib/larch/version.rb
+++ lib/larch/version.rb
@@ -1,6 +1,6 @@
 module Larch
   APP_NAME      = 'Larch'
-  APP_VERSION   = '1.1.0'
+  APP_VERSION   = '1.1.1'
   APP_AUTHOR    = 'Ryan Grove'
   APP_EMAIL     = 'ryan@wonko.com'
   APP_URL       = 'https://github.com/rgrove/larch/'
