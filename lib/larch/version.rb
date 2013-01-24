631dfb53e98a65390cfc53dfbcc62356d7447724
Ryan Grove <ryan@wonko.com>
2013-01-24 15:05:33 -0800

    Release 1.1.2.

--- lib/larch/version.rb
+++ lib/larch/version.rb
@@ -1,9 +1,9 @@
 module Larch
   APP_NAME      = 'Larch'
-  APP_VERSION   = '1.1.1'
+  APP_VERSION   = '1.1.2'
   APP_AUTHOR    = 'Ryan Grove'
   APP_EMAIL     = 'ryan@wonko.com'
   APP_URL       = 'https://github.com/rgrove/larch/'
-  APP_COPYRIGHT = 'Copyright (c) 2011 Ryan Grove <ryan@wonko.com>. All ' <<
+  APP_COPYRIGHT = 'Copyright (c) 2013 Ryan Grove <ryan@wonko.com>. All ' <<
                   'rights reserved.'
 end
